4008545ddc3578a5a2dac517e532abf1fd01308b
RicardoSimple <98212606+RicardoSimple@users.noreply.github.com>
2023-11-02 15:27:15 +0800

    MessageDAO.xml更新

--- web-backend/src/main/java/com/ricardo/web/dao/MessageDAO.java
+++ web-backend/src/main/java/com/ricardo/web/dao/MessageDAO.java
@@ -3,11 +3,15 @@ package com.ricardo.web.dao;
 import com.ricardo.web.dto.MessageDO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface MessageDAO {
     void insertMessage(MessageDO messageDO);
     void updateMessage(MessageDO messageDO);
     MessageDO findMessageById(long Id);
     void deleteMessage(long Id);
+
+    List<MessageDO> findMessageByUserIdAndType(MessageDO messageDO);
 
 }
