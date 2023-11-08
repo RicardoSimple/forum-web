848fdf2fe208673cd7e2c20da48184c4aadd1b61
XB811 <2098780588@qq.com>
2023-11-08 09:06:06 +0800

    修改岗位详情页面，显示公司相关信息 修改个人主页

---
+++ web-client/src/account/userhomepage1_comment.vue
@@ -0,0 +1,33 @@
+
+import SingleComment from '@/components/singleComment.vue';
+
+<template>
+    <div>
+        <div
+        v-for="comment,index in b"
+        :key="index">
+        <singleComment :comment="comment"></singleComment>
+        </div>
+    </div>
+</template>
+<script>
+import singleComment from '@/components/singleComment.vue';
+import { getCommentByIdAndType } from '@/api/commentService';
+export default{
+
+
+    components:{
+        singleComment
+    },
+    async created(){
+        var ras =JSON.parse(sessionStorage.getItem("userData"))
+        var res = (await getCommentByIdAndType(ras.userId,ras.userType)).funData
+        this.comments=res.data;
+    },
+    data(){
+        return{
+            coments:[],
+        }
+    }
+}
+</script>
---
+++ web-client/src/account/userhomepage1_job.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="addartical">
+            1
+        </div>
+    </div>
+</template>
